08925f239dca2b773c95a27546ba43e67cfb4413
Nguyen Manh Huy (FSU2.BU12) <HuyNM3@fsoft.com.vn>
2018-04-02 12:54:43 +0900

    remove old test data
    remove old test data

--- pitalium-junit-attachments/src/test/resources/single/pictures/result.js
+++
@@ -1,1 +0,0 @@
-var resultdata={"com.htmlhifive.pitalium.it.assertion.scroll":{"com.htmlhifive.pitalium.it.assertion.scroll.CompareScrollBorderElementTest":{"compareDifferentBorderWidth[Capabilities [{browser\u003dchrome, platform\u003dVISTA}]]":{"errLocation":"SUCCESS","errName":"SUCCESS","platform":"VISTA","browser":"chrome"}}}}
